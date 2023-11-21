c87e8ce37ed576f94e2fe1b93b9fcd4a515129b6
“Abdul <kaazees31@gmail.com>
2023-11-21 22:49:57 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing required dependencies
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./components/homePage/HomePage";
+import ProfilePage from "./components/profilePage/ProfilePage";
+import LoginPage from "./components/loginPage/loginPage/LoginPage";
+import {useSelector} from "react-redux";
 
+// Main App component
 function App() {
+  // Using the useSelector hook to get the token from the Redux store
+  const isAuth = Boolean(useSelector((state) => state.token)); 
+  // Rendering the app with BrowserRouter and Routes components
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          {/* Defining the routes and their corresponding components */}
+          <Route path = "/" element = {<LoginPage />} />
+          {/* If authenticated, render HomePage component, else navigate to login page */}
+          <Route path = "/home" element = {isAuth ? <HomePage /> : <Navigate  to = "/" />} /> 
+          {/* If authenticated, render ProfilePage component, else navigate to login page */}
+          <Route path = "/profile/:userId" element = {isAuth ? <ProfilePage/> : <Navigate to = "/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
+// Importing required dependencies 
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import authReducer from "./state/index";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from "react-redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH, 
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
+// Persist configuration options
+const persistConfig = {key: "root", storage, version: 1};
+
+// Creating the persisted reducer
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+// Creating the Redux store with the persisted reducer and ignoring certain actions
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+
+// Creating the root element and rendering the App component with the Provider and PersistGate components
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Provider store={store}>
+      <PersistGate loading ={null} persistor = {persistStore(store)}>
+        <App />
+      </PersistGate> 
+    </Provider>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
